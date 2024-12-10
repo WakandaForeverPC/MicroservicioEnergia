a7482f3eb8dbab1eeb574b98d20258393c12c29b
Samuu10 <svallmun@myuax.com>
2024-12-10 18:29:06 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microservicioenergia/MicroservicioEnergiaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioenergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEnergiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioEnergiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microservicioenergia/MicroservicioEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioenergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEnergiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

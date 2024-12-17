5b2c463ec2daff85c7d25cef84ea8ebfc1df789d
inesgmz <igomeolc@myuax.com>
2024-12-17 17:16:36 +0100

    Configuracion del microservicio de Energia, actualizados los controllers de las clases principales

--- src/main/java/com/proyecto/microservicioenergia/controller/EnergiaController.java
+++ src/main/java/com/proyecto/microservicioenergia/controller/EnergiaController.java
@@ -1,24 +1,14 @@
 package com.proyecto.microservicioenergia.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/energia")
+
+@Controller
 public class EnergiaController {
 
-    @GetMapping
-    public String obtenerEnergia() {
-        // Lógica para obtener información de energía
-        return "Información de energía";
-    }
-
-    @PostMapping
-    public String crearEnergia(@RequestBody String nuevaEnergia) {
-        // Lógica para crear nueva información de energía
-        return "Nueva energía creada";
+    @GetMapping("/energia")
+    public String showCity() {
+        return "board-energia";
     }
 }
